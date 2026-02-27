f0bd85b026421a58c4200436fd100aafa7fb9fd9
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2026-02-27 14:38:16 -0300

    Refactor LSP configuration for better organization

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,69 +1,45 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-			ensure_installed = { "clangd", "lua_ls" },
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local on_attach = function(client, bufnr)
-				-- Make keymaps buffer-local so they only exist when an LSP is attached
-				local function map(lhs, rhs, desc)
-					vim.keymap.set("n", lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
-				end
-				map("gd", vim.lsp.buf.definition, "[g]oto [d]efinition")
-				map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
-				map("gr", function()
-					vim.lsp.buf.references({ includeDeclaration = false })
-				end, "[g]oto [r]eferences")
-				map("gi", vim.lsp.buf.implementation, "[g]oto [i]mplementations")
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+      })
 
-				client.server_capabilities.documentFormattingProvider = true
-			end
+      vim.lsp.config("clangd", {
+        cmd = { "clangd", "--background-index", "--clang-tidy" },
+        init_options = {
+          clangdFileStatus = true,
+          fallbackFlags = { "--std=c++98" },
+        },
+      })
 
-			local lsp_flags = {
-				allow_incremental_sync = true,
-				debounce_text_changes = 150,
-			}
+      vim.lsp.enable({ "lua_ls", "clangd" })
 
-			-- Bash
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
-			-- Lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
-			-- C/C++ (clangd)
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				cmd = { "clangd", "--background-index", "--clang-tidy" },
-				init_options = {
-					clangdFileStatus = true,
-					fallbackFlags = { "--std=c++98" },
-				},
-				flags = lsp_flags,
-			})
-		end,
-	},
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local buf = args.buf
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = buf })
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = buf })
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = buf })
+        end,
+      })
+    end,
+  },
 }
