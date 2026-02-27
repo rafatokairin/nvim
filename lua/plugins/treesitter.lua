547777bec2e85f618c987ef033dd0b54638aa786
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2026-02-27 13:57:39 -0300

    Refactor Treesitter configuration setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,14 +2,8 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    -- Pega as configs existentes
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    -- Altera apenas a branch do parser C++
-    parser_config.cpp.install_info.branch = "master"
-
-    -- Setup normal
-    require("nvim-treesitter.config").setup({
+    local config = require("nvim-treesitter.config")
+    config.setup({
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
