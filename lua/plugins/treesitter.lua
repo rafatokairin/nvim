f5822259a9b21d82aa0f0d0cf896eae9de6752e2
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2026-02-26 23:43:41 -0300

    Atualizar o treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,22 +2,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    -- 1. Carrega a lista de parsers ANTES do setup
+    -- Pega as configs existentes
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-    -- 2. Define a regra para a linguagem desejada (ex: 'cpp')
-    -- Isso garante que, quando o auto_install rodar, ele use 'master'
-    parser_config.cpp = {
-      install_info = {
-        url = "https://github.com/tree-sitter/tree-sitter-cpp", 
-        files = { "src/parser.c", "src/scanner.cc" }, -- C++ usa scanner.cc
-        branch = "master", 
-      },
-    }
+    -- Altera apenas a branch do parser C++
+    parser_config.cpp.install_info.branch = "master"
 
-    -- 3. Agora o setup com o auto_install ligado
-    local configs = require("nvim-treesitter.config")
-    configs.setup({
+    -- Setup normal
+    require("nvim-treesitter.config").setup({
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
